e36c19306963bcc003a355ac8d3d08ffa4aa0558
Thiago Pádua <paduathiago@outlook.com>
2022-11-24 11:20:38 -0300

    Commit 13

--- Movie.java
+++ Movie.java
@@ -37,14 +37,14 @@ public class Movie {
     }
   
     public int getFrequentRenterPoints(int daysRented){
-      // add bonus for a two day new release rental
-      if ((getPriceCode() == Movie.NEW_RELEASE) &&
-          daysRented > 1)
-          return 2;
-      return 1;
+        // add bonus for a two day new release rental
+        if ((getPriceCode() == Movie.NEW_RELEASE) &&
+            daysRented > 1)
+            return 2;
+        return 1;
     }
   
     public double getCharge(int daysRented) {
         return _price.getCharge(daysRented);
-    }
+     }
   }
