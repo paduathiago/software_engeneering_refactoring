24feb3545af625f293726272aae17ffa6adeb3b5
Thiago Pádua <paduathiago@outlook.com>
2022-11-24 11:08:27 -0300

    Commit 12

--- Movie.java
+++ Movie.java
@@ -44,24 +44,7 @@ public class Movie {
       return 1;
     }
   
-    public double getCharge(int daysRented){
-        //determine amounts for each line
-        double thisAmount = 0;  
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-            thisAmount += 2;
-            if (daysRented > 2)
-                thisAmount += (daysRented - 2) * 1.5;
-            break;
-            case Movie.NEW_RELEASE:
-            thisAmount += daysRented * 3;
-            break;
-            case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (daysRented > 3)
-                thisAmount += (daysRented - 3) * 1.5;
-                break; 
-        }
-        return thisAmount; 
-       }
+    public double getCharge(int daysRented) {
+        return _price.getCharge(daysRented);
+    }
   }
