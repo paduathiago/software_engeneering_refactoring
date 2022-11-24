17c1b0a2241a429d9afa1507fce766dd0a48259c
Thiago Pádua <paduathiago@outlook.com>
2022-11-24 13:16:30 -0300

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,22 +1,32 @@
 import java.util.Enumeration;
 
 public class HtmlStatement extends Statement {
+    
     public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
-      "</EM></H1><P>\n";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for each rental
-         result += each.getMovie().getTitle()+ ": " +
-         String.valueOf(each.getCharge()) + "<BR>\n";
-      }
-      //add footer lines
-      result += "<P>You owe <EM>" +
-      String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-      result += "On this rental you earned <EM>" + 
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      "</EM> frequent renter points<P>";
-      return result;
+        Enumeration rentals = aCustomer.getRentals();
+        String result = result(aCustomer.getName());
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            //show figures for each rental
+            result += showFigures(each.getMovie().getTitle(), String.valueOf(each.getCharge()));
+        }
+        //add footer lines
+        result += addFooterLines(String.valueOf(aCustomer.getTotalCharge()),
+        String.valueOf(aCustomer.getTotalFrequentRenterPoints()));
+        return result;
+    }
+
+    private String result(String name){
+        return "<H1>Rentals for <EM>" + name + "</EM></H1><P>\n";
+    }
+    private String showFigures(String Title, String Charge)
+    {
+        return Title + ": " + Charge + "<BR>\n";
+    }
+    private String addFooterLines(String Charge, String TotalFrequentRenterPoints){
+        String increment = "";
+        increment += "<P>You owe <EM>" + Charge + "</EM><P>\n";
+        increment += "On this rental you earned <EM>" + TotalFrequentRenterPoints + "</EM> frequent renter points<P>";
+        return increment;
     }
 }
--- TextStatement.java
+++ TextStatement.java
@@ -1,22 +1,32 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
+    
     public String value(Customer aCustomer) {
-      Enumeration rentals = aCustomer.getRentals();
-      String result = "Rental Record for " + aCustomer.getName() +
-      "\n";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         //show figures for this rental
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
-         String.valueOf(each.getCharge()) + "\n";
-      }
-      //add footer lines
-      result += "Amount owed is " +
-      String.valueOf(aCustomer.getTotalCharge()) + "\n";
-      result += "You earned " +
-      String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      " frequent renter points";
-      return result;
-   }
+        Enumeration rentals = aCustomer.getRentals();
+        String result = result(aCustomer.getName());
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            //show figures for this rental
+            result += showFigures(each.getMovie().getTitle(), String.valueOf(each.getCharge()));
+        }
+        //add footer lines
+        result += addFooterLines(String.valueOf(aCustomer.getTotalCharge()),
+        String.valueOf(aCustomer.getTotalFrequentRenterPoints()));
+        return result;
+    }
+   
+    private String result(String name){
+        return "Rental Record for" + name + "\n";
+    }
+    private String showFigures(String Title, String Charge)
+    {
+        return "\t" + Title+ "\t" + Charge + "\n";
+    }
+    private String addFooterLines(String Charge, String TotalFrequentRenterPoints){
+        String increment = "";
+        increment += "Amount owed is " + Charge + "\n";
+        increment += "You earned " + TotalFrequentRenterPoints + " frequent renter points";
+        return increment;
+    }
 }
