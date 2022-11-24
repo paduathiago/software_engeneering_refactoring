e87e2554f3cff6a989db13f2a4051f7074880183
Thiago Pádua <paduathiago@outlook.com>
2022-11-23 23:54:41 -0300

    Commit 3

--- Customer.java
+++ Customer.java
@@ -17,24 +17,24 @@ public class Customer {
       return _name;
    }
 
-  public double amountFor(Rental each){
-      //determine amounts for each line
-      double thisAmount = 0;  
-      switch (each.getMovie().getPriceCode()) {
-         case Movie.REGULAR:
-            thisAmount += 2;
-            if (each.getDaysRented() > 2)
-               thisAmount += (each.getDaysRented() - 2) * 1.5;
-            break;
-         case Movie.NEW_RELEASE:
-            thisAmount += each.getDaysRented() * 3;
-            break;
-         case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (each.getDaysRented() > 3)
-               thisAmount += (each.getDaysRented() - 3) * 1.5;
-             break; 
-      }
+  public double amountFor(Rental aRental){
+    //determine amounts for each line
+    double thisAmount = 0;  
+    switch (aRental.getMovie().getPriceCode()) {
+        case Movie.REGULAR:
+        thisAmount += 2;
+        if (aRental.getDaysRented() > 2)
+            thisAmount += (aRental.getDaysRented() - 2) * 1.5;
+        break;
+        case Movie.NEW_RELEASE:
+        thisAmount += aRental.getDaysRented() * 3;
+        break;
+        case Movie.CHILDRENS:
+        thisAmount += 1.5;
+        if (aRental.getDaysRented() > 3)
+            thisAmount += (aRental.getDaysRented() - 3) * 1.5;
+            break; 
+    }
     return thisAmount; 
   }
   
